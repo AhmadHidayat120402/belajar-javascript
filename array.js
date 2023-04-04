4fef625de774e3d93b13c935299dc3907736e023
AhmadHidayat120402 <ahmadhidayat120402@gmail.com>
2023-04-04 14:26:50 +0700

    dom

---
+++ array.js
@@ -0,0 +1,16 @@
+// penulisan array
+const arraySaya = ['nama','alamat','jenis kelamin'];
+
+const arrayKamu = [];
+arrayKamu[0] = 'dayat'
+arrayKamu[1] = 'zen'
+arrayKamu[2] = 'yayak'
+
+console.log(arrayKamu);
+
+const arrayKita = new Array();
+arrayKita[0]= 'dayat';
+arrayKita[3]='zen';
+
+// pemanggilan array
+console.log(arrayKita[3]);
